2052a2e094ce1c9aee2ca1e8d130e29ce5e9aaee
Ramabhadran <krishner@ub499baf7ddca58bf0b96.ant.amazon.com>
2018-11-21 21:32:23 -0800

    Add new codes

---
+++ ferry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from  bs4 import BeautifulSoup
+import requests
+
+url='https://orca.bcferries.com/cc/marqui/at-a-glance.asp'
+
+data=requests.get(url).text
+soup=BeautifulSoup(data,'html.parser')
+
+#print soup.prettify()
+
+for values in soup.find_all('tr',bgcolor='#dfeaf2'):
+ print(values.td.text)
+ time_per=values.tr
+ for tp in time_per.find_all('td'):
+  print tp.text
+ print('')
