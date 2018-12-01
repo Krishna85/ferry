49d49cc5ddef9f729eb8b365f727ebd0af93849e
Ramabhadran <krishner@ub499baf7ddca58bf0b96.ant.amazon.com>
2018-12-01 14:55:27 -0800

    Fixed a few more logics!

--- ferry.py
+++ ferry.py
@@ -6,6 +6,9 @@ url='https://orca.bcferries.com/cc/marqui/at-a-glance.asp'
 global dict_data
 dict_data={}
 tp_val=[]
+sailings_data=[]
+global temp_data
+temp_data=[]
 list_data=[]
 data=requests.get(url).text
 soup=BeautifulSoup(data,'html.parser')
@@ -13,19 +16,29 @@ soup=BeautifulSoup(data,'html.parser')
 #print soup.prettify()
 
 def make_data():
+ temp_data=[]
+ port_data=[]
+ i=0
  for values in soup.find_all('tr',bgcolor=['#D4E3ED','#dfeaf2']):
-  dict_data['ports']=values.td.text
+  port_data.append(values.td.text)
   time_per=values.tr
   tp_val=[]
   for tp in time_per.find_all('td'):
    tp_val.append(tp.text)
-#  print tp_val
-  dict_data['time']=tp_val[0]
-  dict_data['per']=tp_val[1]
-  print dict_data
-  list_data.append(dict_data)
-  print('--------------------------------------------------------------------')
-  print(list_data)
-  print('*******************************************************************')
+  tp_val=[]
+#  list_data.append(dict_data.copy())
+  for sailings in values.find_all('tr'):
+   sailings_data.append(sailings.text)
+ for item in  sailings_data:
+  if item!='':
+   temp_data.append(item)
+  else:
+    dict_data['sailings']=temp_data
+    dict_data['port']=port_data[i]
+    temp_data=[]
+    list_data.append(dict_data.copy())
+    i=i+1
+ for item in list_data:
+  print item
 if __name__=='__main__':
  make_data()
