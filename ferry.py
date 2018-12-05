b38c87acb044ba97796b694e1f12e1f618a0d6a3
Krishna Ramabhadran <krishner@arista.com>
2018-12-05 10:59:18 -0800

    Flask setup

--- ferry.py
+++ ferry.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 from  bs4 import BeautifulSoup
 import requests
+from flask  import Flask,render_template
+import threading
 
+app=Flask(__name__)
 url='https://orca.bcferries.com/cc/marqui/at-a-glance.asp'
 global dict_data
 dict_data={}
@@ -9,6 +12,7 @@ tp_val=[]
 sailings_data=[]
 global temp_data
 temp_data=[]
+global  list_data
 list_data=[]
 data=requests.get(url).text
 soup=BeautifulSoup(data,'html.parser')
@@ -16,29 +20,37 @@ soup=BeautifulSoup(data,'html.parser')
 #print soup.prettify()
 
 def make_data():
+ threading.Timer(5.0, make_data).start()
+ data=requests.get(url).text
+ soup=BeautifulSoup(data,'html.parser')
  temp_data=[]
  port_data=[]
+ global list_data
+ list_data=[]
  i=0
  for values in soup.find_all('tr',bgcolor=['#D4E3ED','#dfeaf2']):
   port_data.append(values.td.text)
-  time_per=values.tr
-  tp_val=[]
-  for tp in time_per.find_all('td'):
-   tp_val.append(tp.text)
-  tp_val=[]
-#  list_data.append(dict_data.copy())
   for sailings in values.find_all('tr'):
    sailings_data.append(sailings.text)
  for item in  sailings_data:
   if item!='':
-   temp_data.append(item)
+   temp_data.append(item[:item.find('m')+1]+' '+item[item.find('m')+1:])
   else:
     dict_data['sailings']=temp_data
     dict_data['port']=port_data[i]
     temp_data=[]
     list_data.append(dict_data.copy())
     i=i+1
- for item in list_data:
-  print item
+
+@app.route("/")
+def data_out():
+ threading.Timer(5.0, data_out).start()
+ global list_data
+ #for item in list_data:
+ #return item['port']
+  #return item['sailings'][0]
+ return render_template('home.html',data=list_data)
+ list_data=[]
 if __name__=='__main__':
  make_data()
+ app.run()
